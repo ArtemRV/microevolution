a60998d6ad2d54371261094d02ba9104e72cdb3e
Artsiom Rachok <artsiom_rachok@epam.com>
2025-05-31 14:04:54 +0300

    settings change

--- common/settings.py
+++ common/settings.py
@@ -2,20 +2,20 @@
 default_settings = {
     'organism': {
         'radius': 10,
-        'max_speed': 5, # NOTE: max_speed and max_acceleration will likely need significant increase
+        'max_speed': 200,
         'initial_energy': 100,
-        'max_energy': 1000,
-        'max_acceleration': 1, # to compensate for DELTA_TIME in physics calculations (e.g., multiply by ~60).
+        'max_energy': 10000,
+        'max_acceleration': 50,
         'energy_per_food': 20,
-        'energy_per_step': 0.05,
+        'energy_per_step': 0.25,
         'enabled': True,
         'random_start': True,
         'start_radius': 230,
-        'visible_obstacle': 0, # Quantity of obstacle params that will get to model input
-        'visible_food': 3, # Quantity of food params that will get to model input
+        'visible_obstacle': 5, # Quantity of obstacle params that will get to model input
+        'visible_food': 5, # Quantity of food params that will get to model input
         'mass': 1.0,
         'elasticity': 0.5,  # Coefficient of restitution
-        'friction_coefficient': 0.2,
+        'friction_coefficient': 0.0,
     },
     'food': {
         'radius': 5,
@@ -26,14 +26,13 @@ default_settings = {
         'enabled': True,
         'mass': 0.2,
         'elasticity': 0.2,
-        'friction_coefficient': 0.8, # High friction to stop quickly
+        'friction_coefficient': 10.0, # High friction to stop quickly
     },
     'obstacle': {
         'radius': 15,
-        'max_speed': 2, # NOTE: max_speed will likely need significant increase
-                        # to compensate for DELTA_TIME (e.g., multiply by ~60).
+        'max_speed': 100,
         'quantity': 5,
-        'start_quantity': 3,
+        'start_quantity': 5,
         'increment_quantity': 0,
         'enabled': True,
         'mass': 10.0, # Significantly heavier than organism
@@ -57,13 +56,13 @@ default_settings = {
             "enabled": True
         },
         "dish_collision": {
-            "value": -25.0,
+            "value": -2.0,
             "enabled": True,
-            "end_episode": True
+            "end_episode": False
         },
         "obstacle_collision": {
-            "value": -20.0,
-            "enabled": False,
+            "value": -5.0,
+            "enabled": True,
             "end_episode": False
         },
         "energy": {
